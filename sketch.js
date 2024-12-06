55e6b4213ada6e54d524c166f7bab612e57a7e19
city535386 <city535386@gmail.com>
2024-12-06 15:49:51 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://city535386.github.io/frog5555555555/model.json';
 
 
 function preload() {
